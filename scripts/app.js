9f604905cdb05e41264ff1d44c4614dd149c4feb
Traci Williams <ug3000@gmail.com>
2017-04-27 17:57:07 -0500

    working on wedding comment box

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,29 @@
 window.onload = function() {
 	console.log("javascript working for wedding site part 2?")
 }
+
+
+
+/*==============CommentBox==============*/
+
+
+var commentsBox = {
+	comments: ["comment1", "comment2", "comment3"],
+	
+	displayComments: function() {
+		console.log(this.comments)
+	},
+	addComments: function(newComment) {
+		this.comments.push(newComment);
+		this.displayComments();
+	},
+	changeComments: function(indexValue, newComment) {
+		this.comments[indexValue] = newComment;
+		this.displayComments();
+	},
+	deleteComments: function(indexValue) {
+		this.comments.splice(indexValue, 1);
+		this.displayComments();
+	}
+
+};
